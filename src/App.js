012d6e3ab0bd7986ec4f89945958d79eb4f39507
Haris-Aqeel <harisaqeel.2001@gmail.com>
2020-09-19 09:50:09 +0500

    Adding React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-
+import { Routes, Route, BrowserRouter as Router} from 'react-router-dom';
+ 
 function App() {
   return (
     <div className="App">
