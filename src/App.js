56c90e5c427eb1eef5b6b1023ac09f304fc9d500
Haris-Aqeel <harisaqeel.2001@gmail.com>
2020-09-19 11:52:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route, BrowserRouter as Router} from 'react-router-dom';
- 
+import { Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+ import Navbar from './Components/Navbar'
+import Products from  './Pages/Products'
+import AboutUs from  './Pages/AboutUs'
+import ContactUs from  './Pages/ContactUs'
+
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/'>
+              <Navbar/>
+          </Route>
+          <Route path='/products'>
+            <Products/>
+          </Route>
+          <Route path='/about'>
+            <AboutUs/>
+          </Route>
+          <Route path='/contact'>
+            <ContactUs/>
+          </Route>
+        </Switch>
+        </div>
+    </Router>
   );
 }
 
